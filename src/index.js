ec47b25dc3003f9d2dec2760e5bf39e229fbd85b
kenyansa <kenyansajustine@gmail.com>
2022-05-15 04:28:31 +0300

    evaluate the need of state for various components

--- src/index.js
+++ src/index.js
@@ -19,15 +19,17 @@ Step 2: Build A Static Version in React
 
 Step 3: Identify The Minimal (but complete) Representation Of UI State
 
-- The original list of products (don't need state!)
-- The search text the user has entered 
-- The value of the checkbox
-- The filtered list of products
+- The original list of products (don't need state! - props)
+- The search text the user has entered (state)
+- The value of the checkbox (state)
+- The filtered list of products (don't need state! - computed from props + state)
 
-Determining if state is needed:
+
 - Is it passed in from a parent via props? If so, it probably isn’t state.
 - Does it remain unchanged over time? If so, it probably isn’t state.
 - Can you compute it based on any other state or props in your component? If so, it isn’t state.
+
+filteredProduct = products.filter(product => product.name === search)
 
 
 Step 4: Identify Where Your State Should Live
