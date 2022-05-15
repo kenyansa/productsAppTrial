4803bab978754a2d9a09fe5c447d563df4de12ee
kenyansa <kenyansajustine@gmail.com>
2022-05-15 03:52:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+
+function ProductCategoryRow({category}){
+  return (
+    <tr>
+      <td>{category}</td>
+    </tr>
+  )
+}
+
+function ProductRow(price, stocked, name){
+  return (
+    <tr>
+      <td style={{color:  stocked ? "black" : "red" }}>{name}</td>
+      <td>{price}</td>
+      <td>{name}</td>
+    </tr>
+  );
+}
+
+function ProductTable(){
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <ProductCategoryRow category="Sporting Goods" />
+        <ProductRow name="Football" stocked={true} prie={49.99} />
+        <ProductRow name="Baseball" stocked={true} prie={9.99} />
+        <ProductRow name="Basketball" stocked={false} prie={29.99} />
+        <ProductCategoryRow category="Electronics" />
+        <ProductRow name="iPod Touch" stocked={true} prie={49.99} />
+        <ProductRow name="iPhone 5" stocked={false} prie={9.99} />
+        <ProductRow name="Nexus 7" stocked={true} prie={29.99} />
+      </tbody>
+    </table>
+  );
+}
+
+function SearchBar(){
+  return (
+    <form style={{padding: "5px", border: "1px solid blue", display: "flex", flexDirection: "column"}}>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        Only show products in stock
+      </label>
+    </form>
+  )
+}
+
+function FilterableProductTable(){
+  return(
+    <div style={{padding: "5px", border: "1px solid orange", display: "flex", flexDirection: "column"}}>
+      <searchBar />
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -22,4 +85,5 @@ function App() {
   );
 }
 
+ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
