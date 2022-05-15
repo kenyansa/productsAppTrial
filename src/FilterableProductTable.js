b938a4ee8116adcb383df56cc2c7e4ff0ba9d88f
kenyansa <kenyansajustine@gmail.com>
2022-05-15 18:23:34 +0300

    inverse data flow from child to parent components

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -3,13 +3,22 @@ import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
 
 function FilterableProductTable({ products }) {
-    const [search, setSearch] = useState("Football");
-    const [showInStock, setShowInStock] = useState(false)
+  const [search, setSearch] = useState("");
+  const [showInStock, setShowInStock] = useState(false);
 
   return (
     <div>
-      <SearchBar search={search} showInStock = {showInStock} />
-      <ProductTable products={products} search={search} showInStock = {showInStock} />
+      <SearchBar
+        search={search}
+        showInStock={showInStock}
+        onSearchChange={setSearch}
+        onShowStockChange={setShowInStock}
+      />
+      <ProductTable
+        products={products}
+        search={search}
+        showInStock={showInStock}
+      />
     </div>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({ search, showInStock, onSearchChange }) {
   return (
     <form style={{ padding: "10px", display: "flex", flexDirection: "column" }}>
-      <input type="text" placeholder="Search..." />
+      <input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => onSearchChange(e.target.value)}
+      />
       <label>
-        <input type="checkbox" />
+        <input type="checkbox" checked={showInStock} />
         Only show products in stock
       </label>
     </form>
